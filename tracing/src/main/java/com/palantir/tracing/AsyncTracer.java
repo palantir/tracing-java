987bc67bf43e5923ceeaa8e6079de977f213f9fa
Robert Kruszewski <robertk@palantir.com>
2019-03-29 17:43:13 +0000

    tracer

---
+++ tracing/src/main/java/com/palantir/tracing/AsyncTracer.java
@@ -0,0 +1,67 @@
+/*
+ * (c) Copyright 2019 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.tracing;
+
+import java.util.Optional;
+
+public final class AsyncTracer {
+
+    private static final long serialVersionUID = 1L;
+
+    private static final String DEFAULT_OPERATION = "deferred";
+
+    private final Trace deferredTrace;
+    private final String operation;
+
+    public AsyncTracer() {
+        this(Optional.empty());
+    }
+
+    public AsyncTracer(String operation) {
+        this(Optional.of(operation));
+    }
+
+    /**
+     * Create a new deferred tracer, optionally specifying an operation.
+     * If no operation is specified, will attempt to use the parent span's operation name.
+     */
+    public AsyncTracer(Optional<String> operation) {
+        this.operation = operation.orElse(DEFAULT_OPERATION);
+        Tracer.startSpan(this.operation + "-enqueue");
+        deferredTrace = Tracer.copyTrace().get();
+        Tracer.fastDiscardSpan(); // span will completed in the deferred execution
+    }
+
+    /**
+     * Runs the given callable with the current trace at
+     * the time of construction of this {@link DeferredTracer}.
+     */
+    public <T, E extends Throwable> T withTrace(Tracers.ThrowingCallable<T, E> inner) throws E {
+        Trace originalTrace = Tracer.getAndClearTrace();
+        Tracer.setTrace(deferredTrace);
+        // Finish the enqueue span
+        Tracer.fastCompleteSpan();
+        Tracer.startSpan(operation + "-run");
+        try {
+            return inner.call();
+        } finally {
+            // Finish the run span
+            Tracer.fastCompleteSpan();
+            Tracer.setTrace(originalTrace);
+        }
+    }
+}
