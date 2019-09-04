b700633b0c2e32840c33a95aadcd72241a2740d1
Dan Fox <dfox@palantir.com>
2019-09-04 15:42:47 +0100

    nit

--- tracing/src/main/java/com/palantir/tracing/Tracer.java
+++ tracing/src/main/java/com/palantir/tracing/Tracer.java
@@ -93,7 +93,7 @@ public final class Tracer {
     static TraceMetadata getTraceMetadata() {
         Trace trace = checkNotNull(currentTrace.get(), "Unable to getTraceMetadata when there is trace in progress");
 
-        if (isTraceObservable()) {
+        if (trace.isObservable()) {
             OpenSpan openSpan = trace.top()
                     .orElseThrow(() -> new SafeRuntimeException("Trace with no spans in progress"));
             return TraceMetadata.builder()
