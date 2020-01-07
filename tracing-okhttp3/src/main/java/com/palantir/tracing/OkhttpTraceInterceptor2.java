e04c8e1cbb3ccc515b4900fb190286cf67d23baf
Robert Kruszewski <robertk@palantir.com>
2020-01-07 16:10:21 +0000

    imports

--- tracing-okhttp3/src/main/java/com/palantir/tracing/OkhttpTraceInterceptor2.java
+++ tracing-okhttp3/src/main/java/com/palantir/tracing/OkhttpTraceInterceptor2.java
@@ -20,7 +20,6 @@ import com.palantir.logsafe.exceptions.SafeRuntimeException;
 import com.palantir.tracing.api.TraceHttpHeaders;
 import java.io.Closeable;
 import java.io.IOException;
-import java.util.Optional;
 import java.util.function.Function;
 import okhttp3.Interceptor;
 import okhttp3.Request;
