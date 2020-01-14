2e17399f9a99fe17a10dcb61111b8ff2a56d6d14
Carter Kozak <ckozak@ckozak.net>
2020-01-14 12:42:38 -0500

    Provide Tracing support for the full request lifecycle
    Implement TracedRequestHandler which can be applied prior to blocking
    operations in order to capture more of the request lifecycle.
    The existing handler attaches the detached span created by the
    request handler in order to provide more specific timing information
    describing the blocking portion of the request.
    
    For backwards compatibility, the existing TracedOperationHandler
    can still be used without the TracedRequestHandler, however the
    detached request span will be started when the TracedOperationHandler
    is first executed.

---
+++ tracing-undertow/src/main/java/com/palantir/tracing/undertow/TracedRequestHandler.java
@@ -0,0 +1,44 @@
+/*
+ * (c) Copyright 2020 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.tracing.undertow;
+
+import com.palantir.tracing.DetachedSpan;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+
+/**
+ * Extracts Zipkin-style trace information from the given HTTP request and sets up a corresponding {@link
+ * DetachedSpan} to span the entire request.
+ * See <a href="https://github.com/openzipkin/b3-propagation">b3-propagation</a>.
+ *
+ * This handler should be registered as early as possible in the request lifecycle to fully encapsulate
+ * all work.
+ */
+public final class TracedRequestHandler implements HttpHandler {
+
+    private final HttpHandler delegate;
+
+    public TracedRequestHandler(HttpHandler delegate) {
+        this.delegate = delegate;
+    }
+
+    @Override
+    public void handleRequest(HttpServerExchange exchange) throws Exception {
+        UndertowTracing.getOrInitializeRequestTrace(exchange);
+        delegate.handleRequest(exchange);
+    }
+}
