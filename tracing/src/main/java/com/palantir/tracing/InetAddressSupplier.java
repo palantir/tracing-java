b283a07c1714c84470629d13bfb19a3de6cdb9d6
Dan Fox <dfox@palantir.com>
2020-06-26 16:18:37 +0100

    argh

--- tracing/src/main/java/com/palantir/tracing/InetAddressSupplier.java
+++ tracing/src/main/java/com/palantir/tracing/InetAddressSupplier.java
@@ -37,8 +37,8 @@ enum InetAddressSupplier
         try {
             InetAddress candidateAddress = null;
             // Iterate all NICs (network interface cards)...
-            for (Enumeration<NetworkInterface> ifaces =
-                 NetworkInterface.getNetworkInterfaces(); ifaces.hasMoreElements(); ) {
+            for (Enumeration<NetworkInterface> ifaces = NetworkInterface.getNetworkInterfaces();
+                    ifaces.hasMoreElements(); ) {
                 NetworkInterface iface = ifaces.nextElement();
                 // Iterate all IP addresses assigned to each card...
                 for (Enumeration<InetAddress> inetAddrs = iface.getInetAddresses(); inetAddrs.hasMoreElements(); ) {
